1b5489ef119384bff99e244aebc64603de86eeae
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2024-10-03 21:38:36 +0530

    added the prisma adapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,17 @@
-import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
-import prisma from "@/lib/db";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
 
 const handler = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         }),
     ],
-    secret: process.env.NEXTAUTH_SECRET ?? "secret",
-    // callbacks: {
-    //     async signIn({ account, profile }) {
-    //         if (account?.provider === "google") {
-    //             try {
-    //                 const user = await prisma.user.findUnique({
-    //                     where: {
-    //                         email: profile?.email!,
-    //                     },
-    //                 });
-
-    //                 if (!user) {
-    //                     const newUser = await prisma.user.create({
-    //                         data: {
-    //                             email: profile?.email!,
-    //                             name: profile?.name || undefined,
-    //                             provider: "Google",
-    //                         },
-    //                     });
-    //                 }
-    //                 return true;
-    //             } catch (error) {
-    //                 console.log(error);
-    //                 //throw error;
-    //                 return false;
-    //             }
-    //         }
-    //     },
-    // },
 });
 
 export { handler as GET, handler as POST };
