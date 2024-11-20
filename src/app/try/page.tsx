dfff5e9828f40d39f46c3fb19b76370d394a18a9
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2024-11-20 16:49:42 +0530

    Added Try Page

---
+++ src/app/try/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import AppNavBar from "../components/AppNavBar";
+
+export const metadata: Metadata = {
+  title: "Try ELI5 Me",
+  description: "Try Your Personalised Learning Guide Now",
+};
+
+export default function TryPage() {
+  return (
+    <>
+      <AppNavBar />
+      <main className="px-4 pt-6 sm:pt-24 pb-4 bg-gradient-to-b from-blue-50 to-white dark:from-blue-900 dark:to-transparent dark:to-80%">
+        <section className="max-w-screen-xl mx-auto">
+          <div className="space-y-10">
+            <h3 className="text-center text-5xl font-semibold">Try ELI5Me</h3>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
